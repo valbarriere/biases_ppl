84c0537244ee3461e9b57a37503218d52e08e3d0
Sebastian Cifuentes <sebastian.cifuentes@cenia.cl>
2024-03-29 09:16:22 -0300

    Clean biases_calculation  PPL

--- biases_calculation_huggingfacehub_PPL.py
+++ biases_calculation_huggingfacehub_PPL.py
@@ -19,18 +19,17 @@ Author: Valentin Barriere, 02/22
 """
 import os
 import argparse
-from utils import create_input_array, loadTsvData
-from CountryGenderNamePerturbation import PerturbedExamples
 import numpy as np
-from tqdm import tqdm
-from scipy.special import softmax
-from scipy.special import kl_div
-from sklearn.metrics import confusion_matrix
 import pandas as pd
 import pickle as pkl
-import os
+from tqdm import tqdm
 from dotenv import load_dotenv
+from scipy.special import softmax
+from sklearn.metrics import confusion_matrix
 from transformers import TFAutoModelForSequenceClassification, AutoTokenizer, AutoConfig
+
+from utils import create_input_array, loadTsvData
+from CountryGenderNamePerturbation import PerturbedExamples
 
 load_dotenv()
 CACHE_DIR = os.getenv("CACHE_DIR", None)
@@ -38,13 +37,6 @@ PATH_DATA = os.getenv("PATH_DATA", None)
 proxies = None
 
 
-
-
-def _kl_div_scipy(P,Q):
-    """
-    2 options: mean of the KL on each example or mean of the probabilities and KL global 
-    """
-    return np.sum(kl_div(P,Q), axis=1)
 
 def _kl_div(P,Q, mean_of_divs=True):
     """
@@ -64,38 +56,30 @@ def _kl_div(P,Q, mean_of_divs=True):
 
     return kl
 
+
 def symetric_kl(P,Q, mean_of_divs=True):
     return (_kl_div(P,Q,mean_of_divs=mean_of_divs) + _kl_div(Q,P,mean_of_divs=mean_of_divs))/2
 
+
 def prepare_data_and_model_from_scratch(
-    modelFilePath='cardiffnlp/twitter-xlm-roberta-base-sentiment',
-    input_data_File=None, 
+    model_file_path='cardiffnlp/twitter-xlm-roberta-base-sentiment',
+    input_data_file=None, 
     path_corpus=None, 
     model_gen=TFAutoModelForSequenceClassification, 
     tok_gen=AutoTokenizer,
-    task='sentiment',
     ):
     """
+    Load the model, tokenizer, label2id and the tweets with labels
     """
- 
-    tokenizer = tok_gen.from_pretrained(modelFilePath, proxies=proxies, cache_dir = CACHE_DIR)
-    model = model_gen.from_pretrained(modelFilePath, proxies=proxies, cache_dir = CACHE_DIR)
+    tokenizer = tok_gen.from_pretrained(model_file_path, proxies=proxies, cache_dir = CACHE_DIR)
+    model = model_gen.from_pretrained(model_file_path, proxies=proxies, cache_dir = CACHE_DIR)
 
-    config = AutoConfig.from_pretrained(modelFilePath)
-    dict_lab = {v.lower():k for k,v in config.id2label.items()}
+    config = AutoConfig.from_pretrained(model_file_path)
+    dict_lab = {k.lower():v for k,v in config.label2id.items()}
 
-    X_text, y = loadTsvData(os.path.join(path_corpus, input_data_File), dict_lab, multi_labels = False, cumsum_label_vectors = False,)# no_y = True)
+    X_text, y = loadTsvData(os.path.join(path_corpus, input_data_file), dict_lab, multi_labels = False, cumsum_label_vectors = False)
     
     return model, tokenizer, dict_lab, X_text, y
-
-
-def prepare_data_and_model(modelFilePath, model, input_data_File, dict_lab, path_corpus):
-    """
-    """
-    model.load_weights(modelFilePath)
-    X_text, y = loadTsvData(os.path.join(path_corpus, input_data_File), dict_lab, multi_labels = False, cumsum_label_vectors = False,)# no_y = True)
-    
-    return model, X_text, y
 
 
 def sum_pos_neg_emotions(proba):
@@ -113,10 +97,7 @@ def sum_pos_neg_emotions(proba):
     2	optimism
     3	sadness
     """
-    # return cat((proba, proba[:,1:2] + proba[:,2:3], proba[:,0:1] + proba[:,3:4]), axis=1)
     return np.concatenate((proba, proba[:,1:2] + proba[:,2:3], proba[:,0:1] + proba[:,3:4]), axis=1)
-    # return np.concatenate((proba, proba[:,1:2] + proba[:,0:1], proba[:,2:3] + proba[:,3:4]), axis=1)
-    # return np.concatenate((proba, np.sum(proba[:,:2], axis=1), np.sum(proba[:,2:], axis=1)), axis=1)
 
 def create_dict_and_mapping_labels(dict_lab, dict_pos_neg, model_name):
     """
@@ -132,8 +113,6 @@ def create_dict_and_mapping_labels(dict_lab, dict_pos_neg, model_name):
     # used later, when emotion do not calculate the overall 'positive'/'negative' because the code does not work (future todo)
     dict_lab_ini = dict_lab.copy()
     # if emotion
-    # emotion_task = 'anger' in dict_lab.keys()
-    # sentiment_en_task = 'label_0' in dict_lab.keys()
     emotion_task = model_name == "cardiffnlp_roberta-base-tweet-emotion"
     sentiment_en_task = model_name == "cardiffnlp_roberta-base-tweet-sentiment"
     if emotion_task:
@@ -151,14 +130,13 @@ def create_dict_and_mapping_labels(dict_lab, dict_pos_neg, model_name):
 
         # check that all the labels are in the dict_pos_neg so that can be a direct mapping
         list_labels = dict_pos_neg[str_pos]
-        # print(dict_lab, list_labels)
-        assert sum([lab in list(dict_lab.keys()) for lab in list_labels]), "dict_pos_neg does not contain the string names of %s class"%str_pos
+        assert sum([lab in list(dict_lab.keys()) for lab in list_labels]), f"dict_pos_neg does not contain the string names of {str_pos} class"
         for lab in list_labels:
             if lab in list(dict_lab.keys()):
                 str_pos = lab
 
         list_labels = dict_pos_neg[str_neg]
-        assert sum([lab in list(dict_lab.keys()) for lab in list_labels]), "dict_pos_neg does not contain the string names of %s class"%str_neg
+        assert sum([lab in list(dict_lab.keys()) for lab in list_labels]), f"dict_pos_neg does not contain the string names of {str_neg} class"
         for lab in list_labels:
             if lab in list(dict_lab.keys()):
                 str_neg = lab
@@ -169,14 +147,12 @@ def create_dict_and_mapping_labels(dict_lab, dict_pos_neg, model_name):
 
 
 def _calculate_sentiment_bias(model, X_text, y, tokenizer, dict_lab, list_countries=[], n_duplicates=10, 
+                              path_dump_perturbed=None,
                               dict_pos_neg = {'positive' : ['yes', 'non-hate', 'non-offensive', 'joy'], 'negative' : ['no', 'hate', 'offensive', 'sadness']}, 
                               perturb=True,
-                              path_dump_perturbed = '/eos/jeodpp/data/projects/REFOCUS/data/Valentin/Biases/Perturbed_text_stances_en.pkl',
                               model_name='model', use_existing_dic=True, male_only=False, emotion_task=False):
     """
     The function itself, taking model, X_text, y as inputs
-
-    TEST IF WORKING FOR EMOTION WHEN THERE ARE 2 LABELS FOR POS / AND 2 FOR NEG
     """
     
     dict_lab, dict_lab_ini, str_pos, str_neg = create_dict_and_mapping_labels(dict_lab, dict_pos_neg, model_name)
@@ -198,12 +174,12 @@ def _calculate_sentiment_bias(model, X_text, y, tokenizer, dict_lab, list_countr
             pkl.dump(perturbed_X_text, fp)
         
     else:
-        print('Loading already existing %s'%path_dump_perturbed)
+        print(f'Loading already existing {path_dump_perturbed}')
         with open(path_dump_perturbed, 'rb') as fp:
             perturbed_X_text = pkl.load(fp)
     
     nb_sent_modified = len(perturbed_X_text['Original'][0])
-    print('...Obtaining %d perturbated sentences' % nb_sent_modified)
+    print(f'...Obtaining {nb_sent_modified} perturbated sentences')
     
     dict_results = {country : {} for country in perturbed_X_text.keys() if country != 'Original'}
 
@@ -213,7 +189,6 @@ def _calculate_sentiment_bias(model, X_text, y, tokenizer, dict_lab, list_countr
         path_dump_proba_ini = ('/%s/ProbaIni_'%(model_name)).join(os.path.split(path_dump_perturbed)) + '.pkl'
         if not os.path.isfile(path_dump_proba_ini):
             print("Calculating probas...")
-            # print(perturbed_X_text['Original'][0])
 
             input_arrays = create_input_array(perturbed_X_text['Original'][0], tokenizer)
             try: # for TFAutoModelForSequenceClassification
@@ -227,7 +202,7 @@ def _calculate_sentiment_bias(model, X_text, y, tokenizer, dict_lab, list_countr
             with open(path_dump_proba_ini, 'wb') as fp:
                 pkl.dump(proba_ini, fp)
         else:
-            print('loading probaini...')
+            print('Loading ProbaIni...')
             with open(path_dump_proba_ini, 'rb') as f:
                 proba_ini = pkl.load(f)
         
@@ -237,7 +212,6 @@ def _calculate_sentiment_bias(model, X_text, y, tokenizer, dict_lab, list_countr
             y_hat_ini = np.repeat(np.argmax(proba_ini[:,:4], axis=1), n_duplicates)
         else:
             y_hat_ini = np.repeat(np.argmax(proba_ini, axis=1), n_duplicates)
-        # y_hat_ini = [kkk for kk in [n_duplicates*[k] for k in np.argmax(proba_ini, axis=1)] for kkk in kk]
             
         # mean of the difference between pos and neg
         dict_results['OriginalDelta'] = 100*np.mean(proba_ini[:,dict_lab[str_pos]] - proba_ini[:,dict_lab[str_neg]])
@@ -257,7 +231,6 @@ def _calculate_sentiment_bias(model, X_text, y, tokenizer, dict_lab, list_countr
                         else:
                             print(path_dump_proba, 'NOT existing... making it!')
                             input_arrays = create_input_array(examples, tokenizer)
-                            # print(model.predict(input_arrays))
                             try:
                                 proba = softmax(model.predict(input_arrays), axis=1)
                             except: # for TFAutoModelForSequenceClassification
@@ -278,12 +251,6 @@ def _calculate_sentiment_bias(model, X_text, y, tokenizer, dict_lab, list_countr
                             y_hat_pred = np.argmax(proba, axis=1)
                         CM = confusion_matrix(y_hat_ini, y_hat_pred)
 
-                        ## TO CHANGE: use the file, not stance everytime... 
-                        # path_dump_CM = '/eos/jeodpp/data/projects/REFOCUS/data/Valentin/Biases/CM_stances_en'
-                        # with open(path_dump_CM + '_%s.pkl'%country, 'wb') as fp:
-                            # pkl.dump(CM, fp)
-                        ######################
-
                         # gives the percentage of increasing/decreasing 
                         # the number of new prediction - the number of past prediction / the number of past prediction
                         percentage_changing = (np.sum(CM, axis=0)-np.sum(CM, axis=1)) / np.sum(CM, axis=1)
@@ -293,13 +260,11 @@ def _calculate_sentiment_bias(model, X_text, y, tokenizer, dict_lab, list_countr
                             print(percentage_changing)
                             percentage_changing = [1 for _ in dict_lab.values()]
                         
-                        # print(y_hat_ini, y_hat_pred, CM)
                         for lab, idx_lab in dict_lab_ini.items():
                             dict_results[country][gender][lab] = 100*np.round(percentage_changing[idx_lab],3)
 
                         dict_results[country][gender]['KL_sym'] = symetric_kl(np.repeat(proba_ini, n_duplicates, axis=0), proba)
                         dict_results[country][gender]['KL_sym_mean'] = symetric_kl(np.repeat(proba_ini, n_duplicates, axis=0), proba, mean_of_divs=False)
-                        # dict_results[country][gender]['Sinkhorn'] = kl_div(proba_ini, proba)
 
         # creating the df
         del dict_results['OriginalDelta']
@@ -314,41 +279,30 @@ def _calculate_sentiment_bias(model, X_text, y, tokenizer, dict_lab, list_countr
     return df_bias
 
 
-def calculate_sentiment_bias(modelFilePath, model, input_data_File, tokenizer, dict_lab, path_corpus, list_countries=[], n_duplicates=10):
-    """
-    Return the sentiment bias 
-    """
-
-    model, X_text, y = prepare_data_and_model(modelFilePath, model, input_data_File, dict_lab, path_corpus)
-
-    return _calculate_sentiment_bias(model, X_text, y, tokenizer, dict_lab, list_countries=list_countries, n_duplicates=n_duplicates)
-
-
 if __name__ == '__main__':
-
     parser = argparse.ArgumentParser()
-    parser.add_argument("-m", "--model_name", help="The name of the model name", type=str, default="cardiffnlp/twitter-xlm-roberta-base-sentiment", 
-                 )
+    parser.add_argument("-m", "--model_name", help="The name of the model", type=str, default="cardiffnlp/twitter-xlm-roberta-base-sentiment")
     parser.add_argument("-c", "--name_corpora", help="The name of the folder containing the corpora", type=str, 
                         default="Biases")
     parser.add_argument("--path_corpora", help="The path of the folders containing all the corpora", type=str, 
                         default=PATH_DATA)
-    parser.add_argument("--data_tsv", type=str, default="tweets_test_spanish_val.tsv")
-    parser.add_argument("--list_countries", help="countries to test", type=str, default=['United_Kingdom', "France", 'Spain', 'Germany'], nargs='+')
-    parser.add_argument("--n_duplicates", help="how many n_duplicates", type=int, default=10)
+    parser.add_argument("--data_tsv", type=str, help="The tsv file containing the data", default="tweets_test_spanish_val.tsv")
+    parser.add_argument("--list_countries", help="Countries to test", type=str, default=['United_Kingdom', "France", 'Spain', 'Germany'], nargs='+')
+    parser.add_argument("--n_duplicates", help="How many n_duplicates", type=int, default=10)
     parser.add_argument("--test", help="test", default=False, action='store_true')
-    parser.add_argument("--proba_only", help="Proba only", default=False, action='store_true')
-    parser.add_argument("--male_only", help="male only", default=True, type=bool)
+    parser.add_argument("--proba_only", help="Calculate probability only", default=False, action='store_true')
+    parser.add_argument("--male_only", help="Use male only", default=True, action='store_false')
+    parser.add_argument("--perturb", help="Apply perturbation to data", default=True, action='store_false')
+    parser.add_argument("--emotion_task", help="If the task is emotion classification", default=False, action='store_true')
     args = parser.parse_args()
 
-    modelFilePath = args.model_name
+    model_file_path = args.model_name
     path_corpus = os.path.join(args.path_corpora, args.name_corpora)
-    input_data_File = args.data_tsv
+    input_data_file = args.data_tsv
 
+    model, tokenizer, dict_lab, X_text, y = prepare_data_and_model_from_scratch(model_file_path, input_data_file, path_corpus)    
 
-    model, tokenizer, dict_lab, X_text, y = prepare_data_and_model_from_scratch(modelFilePath, input_data_File, path_corpus)    
-
-    model_name_underscore = modelFilePath.replace('/', '_')
+    model_name_underscore = model_file_path.replace('/', '_')
     if args.test:
         X_text = X_text[:200]
     df_bias = _calculate_sentiment_bias(model, X_text, y, tokenizer, dict_lab,
@@ -356,9 +310,10 @@ if __name__ == '__main__':
                                         n_duplicates=args.n_duplicates,
                                         path_dump_perturbed = os.path.join(path_corpus, f'Perturbed_{args.data_tsv}'),
                                         model_name=model_name_underscore,
-                                        male_only=args.male_only)
-    if not args.test:
-        out_file_name = f'{model_name_underscore}biases_{input_data_File}.tsv'
-        df_bias.to_csv(os.path.join(path_corpus, out_file_name), sep='\t')
-    else:
+                                        male_only=args.male_only,
+                                        emotion_task=False)
+    if args.test:
         print(df_bias)
+    else:
+        out_file_name = f'{model_name_underscore}biases_{input_data_file}.tsv'
+        df_bias.to_csv(os.path.join(path_corpus, out_file_name), sep='\t')
